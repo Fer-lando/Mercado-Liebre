f7998d27d2239c242a0e75ea13092fcd8b4266db
Fer-lando <fer.ram91@gmail.com>
2023-01-06 13:42:00 -0300

    puerto

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const app = express();
 const path = require("path");
-const PORT = procces.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static("public"));
 
