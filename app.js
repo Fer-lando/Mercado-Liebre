085b0d4c1c7ee0b22250cfb51704eeca559d9fab
Fer-lando <fer.ram91@gmail.com>
2023-01-06 12:28:39 -0300

    mercado-liebre

---
+++ app.js
@@ -0,0 +1,20 @@
+const express=require("express");
+const app = express();
+const path = require("path");
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.listen(PORT,()=>console.log(`Servidor Corriendo en: ${PORT}`));
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname, "./views/home.html"));
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
